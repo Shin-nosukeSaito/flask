fe947795a1475b35586dac37c8b69d48982e7071
Shin-nosukeSaito <merry.merry.7125@gmail.com>
2019-01-16 21:08:07 +0900

    app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, abort
+from flaski.models import WikiContent
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route("/")
+def hello():
+    contents = WikiContent.query.all()
+    return render_template("index.html", contents=contents)
+
+
+@app.route("/<title>", methods=["GET"])
+def show_content(title):
+    content = WikiContent.query.filter_by(title=title).first()
+    if content is None:
+        abort(404)
+    return render_template("show_content.html",content=content)
+
+if __name__ == "__main__":
+    app.run()
--- datebase.py
+++ datebase.py
@@ -4,12 +4,12 @@ from sqlalchemy.ext.declarative import declarative_base
 import os
 
 database_file = os.path.join(os.path.abspath(os.path.dirname(__file__)),"wiki.db")
-engine = create_engine('sqlite:///' + databese_file, convert_unicode=True)
+engine = create_engine('sqlite:///' + database_file, convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False,bind=engine))
 
 Base = declarative_base()
 Base.query = db_session.query_property()
 
-def init_db()
+def init_db():
     import flaski.models
     Base.metadata.create_all(bind=engine)
